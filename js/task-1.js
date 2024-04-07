72922f39ffee4221cd7a5d5f289742f23884a94a
Plane-Jumper5 <romariobelyaev@gmail.com>
2024-04-07 14:06:22 +0300

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,31 @@
+function isEnoughCapacity(products, containerSize) {
+  // Обчислюємо загальну кількість товарів
+  const totalProducts = Object.values(products).reduce((total, quantity) => total + quantity, 0);
+  
+  // Перевіряємо, чи загальна кількість товарів менше або дорівнює розміру контейнера
+  return totalProducts <= containerSize;
+}
 
 
 
 
 
 
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
 
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
 
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
 
 
 
